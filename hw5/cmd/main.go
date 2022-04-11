97fa26dd45f2509e17bf6141fc5e3200058e6ac0
mishhgun01 <golubev1400@mail.ru>
2022-04-12 01:46:39 +0300

    дз №5

---
+++ hw5/cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go2022/hw5/pkg/crawler"
+	"go2022/hw5/pkg/crawler/spider"
+	"go2022/hw5/pkg/fileworker"
+	"log"
+	"os"
+)
+
+const (
+	urlGoDev     = "https://go.dev/"
+	urlGolangOrg = "https://golang.org/"
+)
+
+func main() {
+
+	var flagVar string
+	var depthVar int
+	flag.StringVar(&flagVar, "s", "", "string to scan")
+	flag.IntVar(&depthVar, "d", 2, "scanning depth")
+	flag.Parse()
+
+	file, err := os.Open("output.txt")
+	if file != nil {
+		data, err := fileworker.ReadFromFile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(data)
+		return
+	}
+	if file == nil {
+		file, err = os.Create("output.txt")
+		if err != nil {
+			log.Fatal("unresolved error:", err)
+		}
+	}
+
+	if err != nil {
+		log.Fatal("unresolved error:", err)
+	}
+	scanner := spider.New()
+	var data []crawler.Document
+	scan, err := scanner.Scan(urlGoDev, depthVar)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data = append(data, scan...)
+	scan, err = scanner.Scan(urlGolangOrg, depthVar)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data = append(data, scan...)
+
+	b, err := fileworker.InputInFile(file, data)
+	if b == 0 || err != nil {
+		log.Fatal("error with writing: ", err)
+	}
+}
