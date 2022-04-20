c919997abafb5b54c952f6792f8ef9de6af233a6
mishhgun01 <AVATARno15>
2022-04-20 23:02:33 +0300

    исправлено

--- hw7/sort_test.go
+++ hw7/sort_test.go
@@ -3,11 +3,14 @@ package hw7
 import (
 	"math/rand"
 	"sort"
-	"strings"
 	"testing"
 	"time"
 )
 
+//отличия тестовых примеров :
+// 1 - подаём на вход обычный слайс разных чисел
+// 2 - слайс с повторяющимися элементами
+// 3 - слайс с повторяющимися элементами и нестандартной записью числа
 func Test_SortInts(t *testing.T) {
 	array := []int{2, 42, 1, 46, 7, 0, 9}
 	sort.Ints(array)
@@ -28,10 +31,10 @@ func Test_SortInts(t *testing.T) {
 		}
 	}
 
-	array = []int{9, 8, 7, 6, 5, 4, 3}
+	array = []int{3, 03, 0, 1, 1, 4, 3}
 	sort.Ints(array)
 	got = array
-	want = []int{3, 4, 5, 6, 7, 8, 9}
+	want = []int{0, 1, 1, 3, 3, 3, 4}
 	for i, v := range want {
 		if got[i] != v {
 			t.Fatalf("Test #3 - got: %+v, want: %+v", got[i], want)
@@ -46,10 +49,10 @@ func Test_SortStrings(t *testing.T) {
 		s    []string
 		want []string
 	}{
-		{"test1", []string{"a", "c", "b"}, []string{"a", "b", "c"}},
-		{"test2", []string{"London", "london", "Aberdeen"}, []string{"Aberdeen", "London", "london"}},
-		{"test3", []string{"aa", "a", ""}, []string{"", "a", "aa"}},
-		{"test4", []string{"3", "1", "2"}, []string{"1", "2", "3"}},
+		{"test1", []string{"a", "c", "b"}, []string{"a", "b", "c"}},                                   // Проверяем сортировкуи букв
+		{"test2", []string{"London", "london", "Aberdeen"}, []string{"Aberdeen", "London", "london"}}, // Проверяем как сортируются заглавные
+		{"test3", []string{"aa", "a", ""}, []string{"", "a", "aa"}},                                   // как сортируется слайс с пустой строкой
+		{"test4", []string{"3", "1", "2"}, []string{"1", "2", "3"}},                                   //как сортируются цифры в строковом виде
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -66,18 +69,32 @@ func Test_SortStrings(t *testing.T) {
 }
 
 func BenchmarkSortInts(b *testing.B) {
-	data := sampleData()
-	sort.Ints(data)
+	for i := 0; i < b.N; i++ {
+		data := sampleData()
+		sort.Ints(data)
+	}
 }
 
-//результат для данного алгоритма - 0.1684 ns/op при сложности О(n)
+//goos: linux
+//goarch: amd64
+//pkg: go2022/hw7
+//cpu: AMD Ryzen 7 3700U with Radeon Vega Mobile Gfx
+//BenchmarkSortInts
+//BenchmarkSortInts-8   	       6	 183576711 ns/op
 
-func BenchmarkSortStrings(b *testing.B) {
-	data := sampleStringData()
-	sort.Strings(data)
+func BenchmarkSortFloat64S(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		data := sampleFloat64Data()
+		sort.Float64s(data)
+	}
 }
 
-//результат для данного алгоритма - 3497976445 ns/op при сложности O(n^2)
+//goos: linux
+//goarch: amd64
+//pkg: go2022/hw7
+//cpu: AMD Ryzen 7 3700U with Radeon Vega Mobile Gfx
+//BenchmarkSortFloat64S
+//BenchmarkSortFloat64S-8   	       3	 342160908 ns/op
 
 func sampleData() []int {
 	rand.Seed(time.Now().UnixNano())
@@ -88,21 +105,11 @@ func sampleData() []int {
 	return data
 }
 
-func sampleStringData() []string {
+func sampleFloat64Data() []float64 {
 	rand.Seed(time.Now().UnixNano())
-	var s []string
-	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-		"abcdefghijklmnopqrstuvwxyz" +
-		"0123456789")
-	length := 8
-	var b strings.Builder
-	for j := 0; j < 100000; j++ {
-		for i := 0; i < length; i++ {
-			b.WriteRune(chars[rand.Intn(len(chars))])
-		}
-		str := b.String()
-		s = append(s, str)
-		str = ""
+	var data []float64
+	for i := 0; i < 1_000_000; i++ {
+		data = append(data, rand.Float64())
 	}
-	return s
+	return data
 }
