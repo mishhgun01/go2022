58cb4ca3647f59d15a6ae156638e45bd22001ae8
mishhgun01 <AVATARno15>
2022-04-27 23:39:30 +0300

    hw10 - переделана согласно заданию - добавлены группы ожидания + теперь за каждого игрока отвечает отдельный канал

--- hw10/cmd/main.go
+++ hw10/cmd/main.go
@@ -3,52 +3,68 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strconv"
+	"sync"
 )
 
-type player struct {
-	name  string
-	score int
-}
-
-func gameplay(game chan string) {
-	var p1, p2 player
-	p1.name = "Mike"
-	p2.name = "Max"
-	p1.score, p2.score = 0, 0
-	var randomHit = 0
-	for {
-		if (p1.score == 11) || (p2.score == 11) {
-			break
-		}
-		game <- p1.name + " pong"
-		randomHit = rand.Intn(4)
-		if randomHit == 0 || randomHit == 1 {
-			p1.score += 1
-			game <- "stop"
-		}
-		game <- p2.name + " pong"
-		randomHit = rand.Intn(4)
-		if randomHit == 0 || randomHit == 1 {
-			p2.score += 1
-			game <- "stop"
-		}
-		if (p1.score == 11) || (p2.score == 11) {
-			break
-		}
-		fmt.Println("________Next round________")
-	}
-	game <- p1.name + " " + strconv.Itoa(p1.score) + ":" + strconv.Itoa(p2.score) + " "
-
-	close(game)
+//gameplay - структура игры с каналами и группами ожидания, а также счётом
+type gameplay struct {
+	waitgroup *sync.WaitGroup
+	scoreOf   map[string]int
+	channel   chan string
 }
 
 func main() {
-	game := make(chan string)
-
-	go gameplay(game)
-
-	for val := range game {
-		fmt.Println(val)
+	game := gameplay{
+		waitgroup: new(sync.WaitGroup),
+		scoreOf:   make(map[string]int),
+		channel:   make(chan string),
 	}
+	game.init()
+}
+
+//init инициализирует игру
+func (g *gameplay) init() {
+	g.waitgroup.Add(2)
+	go g.play("Max")
+	go g.play("Mike")
+	g.channel <- "begin"
+	g.waitgroup.Wait()
+	fmt.Println("Finally, Max:", g.scoreOf["Max"], " Mike:", g.scoreOf["Mike"])
+}
+
+//play отвечает за основную логику игры
+func (g *gameplay) play(name string) {
+	var action string
+	for val := range g.channel {
+		if g.gotWinner() {
+			close(g.channel)
+			g.waitgroup.Done()
+			return
+		}
+		switch val {
+		case "ping":
+			action = "pong"
+		default:
+			action = "ping"
+		}
+		fmt.Println(name, action)
+		if rand.Intn(5) == 0 {
+			g.scoreOf[name] += 1
+			fmt.Println(name, "won serve")
+			g.channel <- "stop"
+		} else {
+			g.channel <- action
+		}
+	}
+	g.waitgroup.Done()
+}
+
+//gotWinner проверяет выиграна ли игра
+func (g *gameplay) gotWinner() bool {
+	for i := range g.scoreOf {
+		if g.scoreOf[i] == 11 {
+			return true
+		}
+	}
+	return false
 }
