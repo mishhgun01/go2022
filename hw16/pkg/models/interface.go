f45f71b0253ffced836ace363766dd1d70bfb820
mishhgun01 <AVATARno15>
2022-06-15 13:51:04 +0300

    fix

--- hw16/pkg/models/interface.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// Интерфейс взаимодействия с БД.
-type Storage interface {
-	Films(id []int) ([]Film, error)
-	DeleteFilm(item Film) error
-	UpdateFilm(item Film) error
-	NewFilm(item Film) (int, error)
-}
