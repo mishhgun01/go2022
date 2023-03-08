fee5c19785f11dfcfa905e697605cd3297b18e19
golub <golubev1400@mail.ru>
2023-03-08 20:36:30 +0300

    review fixes

--- hw18/links-service/pkg/links/links.go
+++ hw18/links-service/pkg/links/links.go
@@ -1,8 +1,27 @@
 package links
 
 import (
-	"strings"
+	"math"
+	"math/rand"
 	"sync"
+)
+
+const (
+	// Набор символов короткого URL
+	shortChars = "abcdefghijklmnopqrstuvwxyz123456789"
+	// Длинна короткого URL
+	urlLen = 6
+)
+
+var (
+	// Максимально возможное число url для заданного набора чимволов и длинны короткой ссылки
+	// При использовании 9 цифр и 26 букы имеем для длинны 6
+	// (9+26)**6 = 1_838_265_625 (1.8 млрд) вариантов
+	maxUrls = int(math.Pow(float64(len([]byte(shortChars))), urlLen))
+)
+
+var (
+	link = "https://test.ru"
 )
 
 type Link struct {
@@ -24,9 +43,23 @@ func New(links []Link) *DB {
 
 func (db *DB) NewLink(url string) string {
 	db.mu.Lock()
+	short := ""
+	for {
+		short = randSeq(urlLen)
+		res := ""
+		for _, shortLink := range db.links {
+			if shortLink.Short == short {
+				res = short
+				break
+			}
+		}
+		if res == "" {
+			break
+		}
+	}
 	link := Link{
 		Link:  url,
-		Short: longToShort(url),
+		Short: short,
 	}
 	db.links = append(db.links, link)
 	db.mu.Unlock()
@@ -35,6 +68,8 @@ func (db *DB) NewLink(url string) string {
 
 func (db *DB) Link(link string) string {
 	var s string
+	db.mu.Lock()
+	defer db.mu.Unlock()
 	for _, v := range db.links {
 		if v.Short == link {
 			s = v.Short
@@ -43,9 +78,12 @@ func (db *DB) Link(link string) string {
 	return s
 }
 
-// Формирование короткой ссылки путём формирования случайного слова из 5 символов.
-func longToShort(long string) string {
-	s := strings.Split(long, "/")
-	short := s[0] + "//" + s[2] + "/"
-	return short
+func randSeq(n int) string {
+	letters := []rune(shortChars)
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
