3c4267ca45eb0bcdb5bf263552d567763f1f6282
golub <golubev1400@mail.ru>
2023-03-08 21:14:52 +0300

    fix short url

--- hw19/links-service/pkg/links/links.go
+++ hw19/links-service/pkg/links/links.go
@@ -1,11 +1,28 @@
 package links
 
 import (
-	"strings"
+	"fmt"
+	"math"
+	"math/rand"
 	"sync"
 )
 
-const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLNMOPQRTTUVWXYZ"
+const (
+	// Набор символов для сокращения ссылки
+	shortChars = "abcdefghijklmnopqrstuvwxyz123456789"
+	// Длинна короткой ссылки
+	urlLen = 6
+)
+
+var (
+	// Максимально возможное число ссылок для заданного набора символов и длины короткой ссылки
+	maxUrls = int(math.Pow(float64(len([]byte(shortChars))), urlLen))
+)
+
+var (
+	// Зарезервированный адрес
+	link = "https://test.ru"
+)
 
 type Link struct {
 	Link  string
@@ -13,22 +30,36 @@ type Link struct {
 }
 
 type DB struct {
-	mu    *sync.Mutex
+	mu    sync.Mutex
 	links []Link
 }
 
 func New(links []Link) *DB {
 	return &DB{
-		mu:    &sync.Mutex{},
+		mu:    sync.Mutex{},
 		links: links,
 	}
 }
 
 func (db *DB) NewLink(url string) string {
 	db.mu.Lock()
+	short := ""
+	for {
+		short = randomString(urlLen)
+		res := ""
+		for _, shortLink := range db.links {
+			if shortLink.Short == short {
+				res = short
+				break
+			}
+		}
+		if res == "" {
+			break
+		}
+	}
 	link := Link{
 		Link:  url,
-		Short: longToShort(url),
+		Short: fmt.Sprintf("%s/%s", link, short),
 	}
 	db.links = append(db.links, link)
 	db.mu.Unlock()
@@ -37,6 +68,8 @@ func (db *DB) NewLink(url string) string {
 
 func (db *DB) Link(link string) string {
 	var s string
+	db.mu.Lock()
+	defer db.mu.Unlock()
 	for _, v := range db.links {
 		if v.Short == link {
 			s = v.Short
@@ -45,8 +78,12 @@ func (db *DB) Link(link string) string {
 	return s
 }
 
-func longToShort(long string) string {
-	s := strings.Split(long, "/")
-	short := s[0] + "//" + s[2] + "/"
-	return short
+func randomString(n int) string {
+	letters := []rune(shortChars)
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
